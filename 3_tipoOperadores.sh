0c23afa3188a0c0ccf5a668e11e95f23cbd2ea34
sarismejiasanchez <sarismejiasanchez@gmail.com>
2023-06-05 21:44:52 -0500

    Curso de Programación en Bash Shell

--- 3_tipoOperadores.sh
+++ 3_tipoOperadores.sh
@@ -13,6 +13,19 @@ echo "Multiplicar A * B = " $((numA * numB))
 echo "Dividir A / B = " $((numA / numB))
 echo "Residuo A % B = " $((numA % numB))
 
-echo -e "Operadores Relacionales"
+echo -e "\nOperadores Relacionales"
 echo "Números: A=$numA y B=$numB"
-echo "Sumar A + B = " $((numA + numB))
+echo "A > B = " $((numA > numB))
+echo "A < B = " $((numA < numB))
+echo "A >= B = " $((numA >= numB))
+echo "A <= B = " $((numA <= numB))
+echo "A == B = " $((numA == numB))
+echo "A != B = " $((numA != numB))
+
+echo -e "\nOperadores de Asignacion"
+echo "Números: A=$numA y B=$numB"
+echo "Sumar A += B = " $((numA += numB))
+echo "Restar A -= B = " $((numA -= numB))
+echo "Multiplicar A *= B = " $((numA *= numB))
+echo "Dividir A /= B = " $((numA /= numB))
+echo "Residuo A %= B = " $((numA %= numB))
